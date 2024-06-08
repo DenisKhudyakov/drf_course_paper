3146851c1096d5604271c24026fd6c9ed4ed8e29
DenisKhudyakov <denis.hudyakov27@gmail.com>
2024-06-08 17:08:53 +0500

    create model user and habit, makemigrations

--- config/settings.py
+++ config/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+
 import os
 from pathlib import Path
+
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -40,10 +42,11 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    #setting_app
+    # setting_app
     "rest_framework",
-    #my_apps
+    # my_apps
     "habit",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +133,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "users.User"

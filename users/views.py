24013a3658ec3b5cb840015dd56d972d2ef9847e
DenisKhudyakov <denis.hudyakov27@gmail.com>
2024-06-13 21:29:37 +0500

    add permission in view UserListCreateView

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from users.models import User
 from users.serializers import UserSerializer
@@ -7,6 +8,13 @@ from users.serializers import UserSerializer
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            self.permission_classes = [IsAuthenticated]
+        elif self.request.method == "POST":
+            self.permission_classes = [AllowAny]
+        return [permission() for permission in self.permission_classes]
 
 
 class UserRetrieveUpdateDestroyView(generics.RetrieveUpdateDestroyAPIView):
